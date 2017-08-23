d2dd7f02450cf7ff4cdcecaa3826fc5ea2a4859a
Pedro De Ona <pdeon003@fiu.edu>
2017-08-22 21:25:28 -0400

    finished building game.rb and game_spec.rb

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -8,7 +8,56 @@ module TicTacToe
     end
 
     def switch_players
-      @current_player, @other_player = @other_player, @current_player
+      players = [self.current_player, self.other_player]
+      @other_player = players[0]
+      return @current_player = players[1]
+    end
+
+    def solicit_move
+      "#{current_player.name}: Enter a number between 1 and 9 to make your move"
+    end
+
+    def get_move(human_move = gets.chomp)
+      human_move_to_coordinate(human_move)
+    end
+
+    def game_over_message
+      return "#{current_player.name} won!" if board.game_over == :winner
+      return "The game ended in a tie" if board.game_over == :draw
+    end
+
+    def play
+      puts "#{current_player.name} has randomly been selected as the first player"
+      begin
+        board.grid
+        puts ""
+        puts solicit_move
+        x, y = get_move
+        board.set_cell(x, y, current_player.color)
+        if board.game_over
+          puts game_over_message
+          p board.grid
+          return
+        else
+          switch_players
+        end
+      end while !(board.game_over.nil?)
+    end
+    private
+ 
+    def human_move_to_coordinate(human_move)
+      mapping = {
+        "1" => [0, 0],
+        "2" => [1, 0],
+        "3" => [2, 0],
+        "4" => [0, 1],
+        "5" => [1, 1],
+        "6" => [2, 1],
+        "7" => [0, 2],
+        "8" => [1, 2],
+        "9" => [2, 2]
+      }
+      mapping[human_move]
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,7 +5,7 @@ module TicTacToe
     let (:bob) { Player.new({color: "X", name: "bob"}) }
     let (:frank) { Player.new({color: "O", name: "frank"}) }
     let (:game) do
-      player_array = Array.new.<<(frank).<<(bob)
+      player_array = [frank, bob]
       game = Game.new(player_array)
     end
  
@@ -22,12 +22,46 @@ module TicTacToe
     context "#switch_players" do
       it "will set @current_player to @other_player" do
         game.switch_players
-        expect(game.current_player.name).to eq other_player.name
+        expect(game.current_player.name).not_to eq game.other_player.name
       end
-     
+
       it "will set @other_player to @current_player" do
         game.switch_players
-        expect(game.other_player).to eq current_player.name
+        expect(game.other_player.name).not_to eq game.current_player.name
+      end
+    end
+
+    context "#solicit_move" do
+      it "asks the player to make a move" do
+        game = Game.new([bob, frank])
+        expected = "#{game.current_player.name}: Enter a number between 1 and 9 to make your move"
+        expect(game.solicit_move).to eq expected
+      end
+    end
+    
+    context "#get_move" do
+      it "converts human_move of '1' to [0, 0]" do
+        game = Game.new([bob, frank])
+        expect(game.get_move("1")).to eq [0, 0]
+      end
+     
+      it "converts human_move of '7' to [0, 2]" do
+        game = Game.new([bob, frank])
+        expect(game.get_move("7")).to eq [0, 2]
+      end
+    end
+
+    context "#game_over_message" do
+      it "returns '{current player name} won!' if board shows a winner" do
+        game = Game.new([bob, frank])
+        allow(game.board).to receive(:game_over) { :winner }
+        expect(game.game_over_message).to eq "#{game.current_player.name} won!"
+      end
+     
+      it "returns 'The game ended in a tie' if board shows a draw" do
+        game = Game.new([bob, frank])
+        allow(game.board).to receive(:game_over) { :draw }
+        expect(game.game_over_message).to eq "The game ended in a tie"
       end
     end
   end
